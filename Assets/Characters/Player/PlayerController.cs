410eed66709e4a8f06175fe802b9f42ae8c6d105
duykhanh <ninggiangboy@gmail.com>
2024-06-15 12:42:40 +0700

    first commit

---
+++ Assets/Characters/Player/PlayerController.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Characters.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
+        private static readonly int Defeat = Animator.StringToHash("Defeat");
+        [Range(0.1f, 1.5f)] public float moveSpeed = 1f;
+        [Range(0f, 0.1f)] public float collisionOffset = 0.05f;
+        public ContactFilter2D movementFilter;
+        public int maxHealth = 100;
+        public int currentHealth;
+        private readonly List<RaycastHit2D> _castCollisions = new();
+        private Animator _animator;
+        private bool _canMove = true;
+        private Vector2 _inputMovement;
+        private Rigidbody2D _rb;
+        private SpriteRenderer _spriteRenderer;
+        private HealthBarController healthBar;
+        public float pushBackForce = 1f;
+
+        private GameObject _swordHitBox;
+        // Start is called before the first frame update 
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _swordHitBox = transform.Find("SwordHitBox").gameObject;
+            healthBar = GameObject.Find("HealthBar").GetComponent<HealthBarController>();
+            healthBar.SetMaxHealth(maxHealth);
+            currentHealth = maxHealth;
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            if (_canMove && _inputMovement != Vector2.zero)
+            {
+                var success = TryMove(_inputMovement);
+
+                if (!success) success = TryMove(new Vector2(_inputMovement.x, 0));
+
+                if (!success) success = TryMove(new Vector2(0, _inputMovement.y));
+
+                _animator.SetBool(IsMoving, success);
+                _spriteRenderer.flipX = _inputMovement.x < 0;
+                _animator.SetFloat(Horizontal, _inputMovement.x);
+                _animator.SetFloat(Vertical, _inputMovement.y);
+            }
+            else
+            {
+                _animator.SetBool(IsMoving, false);
+            }
+        }
+
+        private void OnMove(InputValue value)
+        {
+            _inputMovement = value.Get<Vector2>();
+        }
+
+        private void OnFire()
+        {
+            _animator.SetTrigger(Attack);
+        }
+
+        private bool TryMove(Vector2 direction)
+        {
+            var count = _rb.Cast(
+                direction,
+                movementFilter,
+                _castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            if (count != 0) return false;
+            _rb.MovePosition(_rb.position + direction * (moveSpeed * Time.fixedDeltaTime));
+            return true;
+        }
+
+        
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                TakeDamage(10);
+            }
+        }
+
+        void Defeated()
+        {
+            _animator.SetTrigger(Defeat);
+        }
+
+        public void EndGame()
+        {
+            Time.timeScale = 0;
+        }
+
+        private void LockMovement()
+        {
+            _swordHitBox.SetActive(true);
+            _canMove = false;
+        }
+
+        private void UnlockMovement()
+        {
+            _swordHitBox.SetActive(false);
+            _canMove = true;
+        }
+
+        private void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            // _rb.AddForce(-_inputMovement * pushBackForce, ForceMode2D.Impulse); not work
+            if (currentHealth <= 0)
+            {
+                Defeated();
+            }
+        }
+    }
+}
---
+++ Assets/Characters/Slime/SlimeController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeController : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    public float moveSpeed = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").transform; 
+        _rb = GetComponent<Rigidbody2D>();     
+        _animator = GetComponent<Animator>();  
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (playerTransform != null) // Check if player exists
+        {
+            // Calculate direction towards player
+            Vector2 direction = playerTransform.position - transform.position;
+            direction.Normalize(); // Normalize the direction vector to have a magnitude of 1
+            // Move the enemy towards the player
+            _rb.MovePosition(_rb.position + direction * (moveSpeed * Time.fixedDeltaTime));
+            // Flip the enemy sprite based on the direction
+            _spriteRenderer.flipX = direction.x < 0;
+            _animator.SetBool("IsMoving", direction.magnitude > 0);
+        }
+    }
+}
